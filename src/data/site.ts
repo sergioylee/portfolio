aa2e79553079a7166c728c02ef044c4f08bbd5d6
Sergio Yunsuk Lee <sergioleesp@gmail.com>
2026-03-28 20:29:43 +0100

    update mail

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,7 +1,7 @@
 export const site = {
   url: "https://sergioylee.com",
   name: "Sergio Y. Lee",
-  email: "sergioleesp@gmail.com",
+  email: "business@sergioylee.com",
   github: "sergioylee",
   linkedin: "sergioylee",
   location: "Madrid, Spain",
