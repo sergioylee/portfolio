23e9d41a8d68d890ea40ff22f67a0a4ab917fdcf
Sergio Yunsuk Lee <sergioleesp@gmail.com>
2026-03-29 00:03:14 +0100

    feat blogs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { site } from "./src/data/site.ts";
 // https://astro.build/config
 export default defineConfig({
   site: site.url,
+  server: { port: 3000 },
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
